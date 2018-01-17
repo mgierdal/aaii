a6741ec0788b63038523d10e5b96d309aaed686e
Marcin Gierdalski <mgierdal@gmail.com>
2018-01-16 23:09:22 -0500

    update

--- screens.py
+++ screens.py
@@ -6,13 +6,14 @@ os.chdir('.')
 
 import requests
 from secrets import ID, PASSWORD
+import shutil
 
-LOGIN_URL = r'http://www.aaii.com/stockideas?a=menubarHome'
+STOCK_IDEAS_URL = r'http://www.aaii.com/stockideas?a=menubarHome'
 
 # all screens
 PERFORMANCE_ROOT    = r'http://www.aaii.com/stockideas/performance'
 PERFORMANCE_MONTHLY = r'http://www.aaii.com/files/spreadsheets/stockideas/monthlyperformance.xlsx'
-PERFORMANCE_ANUAL   = r'http://www.aaii.com/files/spreadsheets/stockideas/annualperformance.xlsx'
+PERFORMANCE_ANNUALLY   = r'http://www.aaii.com/files/spreadsheets/stockideas/annualperformance.xlsx'
 
 # individual screens
 r'http://www.aaii.com/stockideas/screendata/CashRichFirms'
@@ -26,15 +27,30 @@ payload = {
 
 url = LOGIN_URL
 
+def download_file(url):
+    """
+    from https://stackoverflow.com/questions/16694907/how-to-download-large-file-in-python-with-requests-py
+    """
+    local_filename = url.split('/')[-1]
+    r = requests.get(url, stream=True)
+    with open(local_filename, 'wb') as f:
+        shutil.copyfileobj(r.raw, f)
+
+    return local_filename
+
 ##################################
 
 # Use 'with' to ensure the session context is closed after use.
 with requests.Session() as s:
-    p = s.post(url, data=payload)
+    stock_ideas = s.post(STOCK_IDEAS_URL, data=payload)
     # print the html returned or something more intelligent to see if it's a successful login page.
-    print (p.text)
+    #print (p.text)
+
+    performance = s.post(PERFORMANCE_ROOT, data=payload)
 
     # An authorised request.
-    r = s.get(PERFORMANCE_MONTHLY)
+    #r = s.get(PERFORMANCE_MONTHLY)
     #print (r.text)
-        # etc...
+    
+    download_file(PERFORMANCE_MONTHLY)
+    download_file(PERFORMANCE_ANNUALLY)
